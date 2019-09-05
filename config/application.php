7b1bd34399360f1935234ae8e3ed8612861fb081
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-09-05 08:56:22 +0700

    Move multisite config to config/application.php file
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- config/application.php
+++ config/application.php
@@ -102,6 +102,21 @@ Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', 0);
 
 /**
+ * Multisite Settings
+ */
+Config::define('WP_ALLOW_MULTISITE', env('WP_ALLOW_MULTISITE'));
+$multisite = env('MULTISITE') ?: false;
+
+if ($multisite) {
+    Config::define('MULTISITE', $multisite);
+    Config::define('SUBDOMAIN_INSTALL',    env('SUBDOMAIN_INSTALL') ?: false);
+    Config::define('DOMAIN_CURRENT_SITE',  env('DOMAIN_CURRENT_SITE'));
+    Config::define('PATH_CURRENT_SITE',    env('PATH_CURRENT_SITE') ?: '/');
+    Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
+    Config::define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
+}
+
+/**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
  * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
  */
--- config/environments/development.php
+++ config/environments/development.php
@@ -15,15 +15,3 @@ ini_set('display_errors', 1);
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
-
-/**
- * Multisite Settings
- */
-// Config::define('WP_ALLOW_MULTISITE', true);
-Config::define('MULTISITE', true);
-Config::define('SUBDOMAIN_INSTALL', true);
-
-Config::define('DOMAIN_CURRENT_SITE',  env('DOMAIN_CURRENT_SITE'));
-Config::define('PATH_CURRENT_SITE',    env('PATH_CURRENT_SITE') ?: '/');
-Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
-Config::define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
