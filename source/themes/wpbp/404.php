94dea522c467d9cd95a18aa55788359c7ecc756d
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-09-25 08:11:30 +0700

    Fix 404 page
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- source/themes/wpbp/404.php
+++ source/themes/wpbp/404.php
@@ -9,38 +9,6 @@
  * @since      0.1.0
  */
 
-?><section id="main" class="site-main error-404 not-found">
-	<div class="page-content">
-		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wpbp' ); ?></p>
-
-		<?php
-		get_search_form();
-
-		the_widget( 'WP_Widget_Recent_Posts' );
-		?>
-
-		<div class="widget widget_categories">
-			<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wpbp' ); ?></h2>
-			<ul>
-				<?php
-				wp_list_categories( [
-					'orderby'    => 'count',
-					'order'      => 'DESC',
-					'show_count' => 1,
-					'title_li'   => '',
-					'number'     => 10,
-				] );
-				?>
-			</ul>
-		</div> <!-- .widget -->
-
-		<?php
-		/* translators: %1$s: smiley */
-		$wpbp_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wpbp' ), convert_smilies( ':)' ) ) . '</p>';
-		the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$wpbp_archive_content" );
-
-		the_widget( 'WP_Widget_Tag_Cloud' );
-		?>
-
-	</div> <!-- .page-content -->
+?><section id="main" class="site-main content error-404 not-found">
+	<?php get_template_part( 'templates/content', 'none' ); ?>
 </section> <!-- .error-404 -->
--- source/themes/wpbp/templates/content-none.php
+++ source/themes/wpbp/templates/content-none.php
@@ -9,41 +9,37 @@
  * @since      0.1.0
  */
 
-?><section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'wpbp' ); ?></h1>
-	</header> <!-- .page-header -->
-
+?><section <?php post_class(); ?>>
 	<div class="page-content">
-		<p>
-			<?php
-			if ( is_home() && current_user_can( 'publish_posts' ) ) {
+		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wpbp' ); ?></p>
 
-				printf(
-					wp_kses(
-						/* translators: 1: link to WP admin new post page. */
-						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'wpbp' ),
-						[
-							'a' => [
-								'href' => [],
-							],
-						]
-					),
-					esc_url( admin_url( 'post-new.php' ) )
-				);
+		<?php
+		get_search_form();
 
-			} elseif ( is_search() ) {
+		the_widget( 'WP_Widget_Recent_Posts' );
+		?>
 
-				esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'wpbp' );
+		<div class="widget widget_categories">
+			<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wpbp' ); ?></h2>
+			<ul>
+				<?php
+				wp_list_categories( [
+					'orderby'    => 'count',
+					'order'      => 'DESC',
+					'show_count' => 1,
+					'title_li'   => '',
+					'number'     => 10,
+				] );
+				?>
+			</ul>
+		</div> <!-- .widget -->
 
-			} else {
+		<?php
+		/* translators: %1$s: smiley */
+		$wpbp_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wpbp' ), convert_smilies( ':)' ) ) . '</p>';
+		the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$wpbp_archive_content" );
 
-				esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'wpbp' );
-
-			}
-			?>
-		</p>
-
-		<?php get_search_form(); ?>
+		the_widget( 'WP_Widget_Tag_Cloud' );
+		?>
 	</div> <!-- .page-content -->
 </section> <!-- .no-results -->
