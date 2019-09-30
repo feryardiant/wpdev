9d43f362e3ed3a97814426b6f14abf07ebe01ae9
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-09-30 14:09:57 +0700

    chore(heroku): install multisite by default
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- config/heroku/postinstall.sh
+++ config/heroku/postinstall.sh
@@ -2,11 +2,13 @@
 cd $HOME
 
 if [ ! -z $HEROKU_APP_NAME ]; then
-    vendor/bin/wp core install \
+    wp core multisite-install --skip-email --no-color
         --url="${HEROKU_APP_NAME}.herokuapp.com" --title="WordPress Site" \
         --admin_user="admin" --admin_password="secret" --admin_email="admin@example.com" \
-        --skip-email --no-color
 
-    wp option update permalink_structure '/%postname%/'
+    if [ `wp option update permalink_structure` != '/%postname%/' ]; then
+        wp option update permalink_structure '/%postname%/'
+    fi
+
     # wp option update link_manager_enabled '1'
 fi
