64ec3c44a8d5603ea6f3eb921cfc73e04fdd6876
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-02 02:00:46 +0700

    chore: make WP_DEBUG_LOG configurable thru .env
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- config/application.php
+++ config/application.php
@@ -128,6 +128,7 @@ Config::define('DISALLOW_FILE_MODS',  env('DISALLOW_FILE_MODS'));
  */
 Config::define('WP_DEBUG',         env('WP_DEBUG') ?: false);
 Config::define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?: false);
+Config::define('WP_DEBUG_LOG',     env('WP_DEBUG_LOG') ?: false);
 Config::define('SCRIPT_DEBUG',     env('SCRIPT_DEBUG') ?: false);
 
  /**
