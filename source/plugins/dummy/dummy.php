fe342aaf0d524fbd5d5045de2c4d259226681def
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-04 14:19:03 +0700

    chore: remove unecessary plugins
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- source/plugins/dummy/dummy.php
+++ source/plugins/dummy/dummy.php
@@ -7,7 +7,7 @@
  * Author URI:   https://feryardiant.id/
  * Text Domain:  dummy
  * Domain Path:  /languages
- * Version:      0.1.0
+ * Version:      0.2.0
  *
  * @package     WP Theme Dev
  * @subpackage  Dummy Plugin
