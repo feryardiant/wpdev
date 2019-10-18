db15503ad2954de1c4d8d013847766cb64d16795
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-18 07:05:03 +0700

    refactor(plugins): rename dummy plugin name
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ source/plugins/blank-companion/functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Blank Companion
+ * Plugin URI:  https://wp.feryardiant.id
+ * Description: A WordPress Plugin that actually doing nothing
+ * Version:     0.2.0
+ * Author:      Fery Wardiyanto
+ * Author URI:  https://feryardiant.id/
+ * Text Domain: blank
+ *
+ * @package     WP Theme Dev
+ * @subpackage  Blank Plugin
+ * @since       0.1.0
+ */
+
+// Your code starts here.
