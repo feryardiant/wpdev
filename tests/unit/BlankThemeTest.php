db98603892a387f8ef9222c430057bdd9a1b79bf
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-20 07:43:10 +0700

    test(theme): add some tests
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/unit/BlankThemeTest.php
+++ tests/unit/BlankThemeTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests;
 
+use Blank\Feature;
 use Blank\Theme;
+use Closure;
+use InvalidArgumentException;
 use ReflectionClass;
+use RuntimeException;
 use WP_Mock as mock;
 
 class BlankThemeTest extends TestCase {
@@ -18,8 +22,17 @@ class BlankThemeTest extends TestCase {
         'author_uri' => 'http://example.com',
     ];
 
-    protected function new_instance_without_constructor(string $class_name) {
+    protected function new_instance_without_constructor(string $class_name, Closure $callback = null) {
         $reflector = new ReflectionClass($class_name);
+
+        $this->transient['parent_dir'] = BASE_PATH . '/source/themes/blank';
+        $this->transient['parent_uri'] = $this->transient['siteurl'] . '/wp-content/themes/blank';
+        $this->transient['child_dir']  = $this->transient['parent_dir'];
+        $this->transient['child_uri']  = $this->transient['parent_uri'];
+
+        if ($callback) {
+            $callback($reflector);
+        }
 
         return $reflector->newInstanceWithoutConstructor();
     }
@@ -28,5 +41,57 @@ class BlankThemeTest extends TestCase {
         $theme = $this->new_instance_without_constructor(Theme::class);
 
         $this->assertInstanceOf(Theme::class, $theme);
+    }
+
+    public function test_transient_name() {
+        $theme = $this->new_instance_without_constructor(Theme::class, function (ReflectionClass $theme) {
+            $prop = $theme->getProperty('cached');
+            $prop->setAccessible(true);
+            $prop->setValue($theme, (object) []);
+        });
+
+        $this->assertEquals('blank_theme_info', $theme->transient_name('theme_info'));
+    }
+
+    public function test_setter_and_getter() {
+        $theme = $this->new_instance_without_constructor(Theme::class, function (ReflectionClass $theme) {
+            $prop = $theme->getProperty('cached');
+            $prop->setAccessible(true);
+            $prop->setValue($theme, (object) [
+                'theme_info' => $this->transient,
+            ]);
+        });
+        
+        $this->assertNull($theme->dummy);
+
+        $theme->dummy = DummyFeature::class;
+
+        $this->assertInstanceOf(DummyFeature::class, $theme->dummy);
+
+        $this->assertEquals($this->transient['siteurl'], $theme->siteurl);
+    }
+
+    public function test_setter_error_on_non_feature_subclass() {
+        $theme = $this->new_instance_without_constructor(Theme::class);
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $theme->foo = new class {};
+    }
+
+    public function test_setter_error_on_non_feature_instance() {
+        $theme = $this->new_instance_without_constructor(Theme::class);
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $theme->foo = function () {
+            return new class {};
+        };
+    }
+}
+
+class DummyFeature extends Feature {
+    public function initialize() : void {
+        // .
     }
 }
