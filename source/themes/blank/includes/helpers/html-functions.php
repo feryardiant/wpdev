342d1d81a0756d5ae8620c897b3070d0c0da0965
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-21 06:16:13 +0700

    feat(theme): add Schema.org helper function
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- source/themes/blank/includes/helpers/html-functions.php
+++ source/themes/blank/includes/helpers/html-functions.php
@@ -142,11 +142,108 @@ function get_allowed_attr( $tag, array $attr = [] ) : array {
 		$allowed_kses = wp_kses_allowed_html( 'post' );
 	}
 
+	$schema_org = [
+		'itemscope' => 1,
+		'itemprop'  => 1,
+		'itemtype'  => 1,
+	];
+
 	if ( array_key_exists( $tag, $allowed_kses ) ) {
-		return $allowed_kses[ $tag ];
+		return array_merge( $allowed_kses[ $tag ], $schema_org );
 	}
+
+	$attr = array_merge( array_flip( array_keys( $attr ) ), $schema_org );
 
 	return array_map( function () {
 		return 1;
-	}, array_flip( array_keys( $attr ) ) );
+	}, $attr );
+}
+
+/**
+ * Retrieve Schema.org attributes array for given $context.
+ *
+ * @link https://schema.org/docs/gs.html
+ * @param  string $context
+ * @return array
+ */
+function get_schema_org_attr( string $context ) : array {
+	$attr = [ 'itemscope' => null ];
+
+	switch ( $context ) {
+		case 'header':
+			$attr['itemtype'] = 'http://schema.org/WPHeader';
+			return $attr;
+
+		case 'logo':
+			$attr['itemtype'] = 'http://schema.org/Brand';
+			return $attr;
+
+		case 'navigation':
+			$attr['itemtype'] = 'http://schema.org/SiteNavigationElement';
+			return $attr;
+
+		case 'blog':
+			$attr['itemtype'] = 'http://schema.org/Blog';
+			return $attr;
+
+		case 'breadcrumb':
+			$attr['itemtype'] = 'http://schema.org/BreadcrumbList';
+			return $attr;
+
+		case 'breadcrumb_list':
+			$attr['itemprop'] = 'itemListElement';
+			$attr['itemtype'] = 'http://schema.org/ListItem';
+			return $attr;
+
+		case 'breadcrumb_itemprop':
+			$attr['itemprop'] = 'breadcrumb';
+			return $attr;
+
+		case 'sidebar':
+			$attr['itemtype'] = 'http://schema.org/WPSideBar';
+			return $attr;
+
+		case 'footer':
+			$attr['itemtype'] = 'http://schema.org/WPFooter';
+			return $attr;
+
+		case 'headline':
+			$attr['itemprop'] = 'headline';
+			return $attr;
+
+		case 'entry_content':
+			$attr['itemprop'] = 'text';
+			return $attr;
+
+		case 'publish_date':
+			$attr['itemprop'] = 'datePublished';
+			return $attr;
+
+		case 'author_name':
+			$attr['itemprop'] = 'name';
+			return $attr;
+
+		case 'author_link':
+			$attr['itemprop'] = 'author';
+			return $attr;
+
+		case 'item':
+			$attr['itemprop'] = 'item';
+			return $attr;
+
+		case 'url':
+			$attr['itemprop'] = 'url';
+			return $attr;
+
+		case 'position':
+			$attr['itemprop'] = 'position';
+			return $attr;
+
+		case 'image':
+			$attr['itemprop'] = 'image';
+			return $attr;
+
+		default:
+			return [];
+	}
 }
