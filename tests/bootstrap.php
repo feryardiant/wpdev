47efac0e6b0e21030ecfe95c2c406b9edef2a956
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-20 07:44:12 +0700

    test: define a constant for root directory
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,10 @@
  * @package Sample
  */
 
-$root = dirname(__DIR__);
+defined('BASE_PATH') || define('BASE_PATH', dirname(__DIR__));
 
-require_once $root.'/vendor/autoload.php';
+require_once BASE_PATH.'/vendor/autoload.php';
 
 WP_Mock::bootstrap();
 
-require_once $root.'/source/themes/blank/includes/autoload.php';
+require_once BASE_PATH.'/source/themes/blank/includes/autoload.php';
