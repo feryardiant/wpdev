bdb8acf9c9d917592b8813bb2a11aaedab86f089
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-22 21:31:14 +0700

    feat(deploy): update postinstall script, WIP
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- config/heroku/postinstall.sh
+++ config/heroku/postinstall.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 cd $HOME
 
+curl -Lso vendor/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x vendor/bin/wp
+
 if [ ! -z $HEROKU_APP_NAME ]; then
-    wp core install --skip-email --no-color \
+    vendor/bin/wp core install --skip-email --no-color \
         --url="https://${HEROKU_APP_NAME}.herokuapp.com" --title="WordPress Site" \
         --admin_user="admin" --admin_password="secret" --admin_email="admin@example.com"
 
     # wp option update permalink_structure '/%postname%/'
     # wp option update link_manager_enabled '1'
 fi
+
+APP_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
