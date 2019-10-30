6a4c6f70058456d6c9b484267f8de8daafd327f3
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-30 07:16:33 +0700

    docs(theme): another documentation update
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- source/themes/blank/functions.php
+++ source/themes/blank/functions.php
@@ -13,13 +13,19 @@
  */
 require_once __DIR__ . '/includes/autoload.php';
 
+/**
+ * Initiate theme instance.
+ */
 $blank_theme = new Blank\Theme();
 
+/**
+ * Load theme options from `options` directory.
+ */
 $blank_theme->load_options();
 
 if ( ! function_exists( 'blank' ) ) {
 	/**
-	 * Main theme object.
+	 * Helper function to interact with theme instance.
 	 *
 	 * @param string|null $key Theme object key.
 	 * @return Blank\Theme
