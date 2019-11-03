1297cbd84c67c6fe7259fdc1e6110bdf0f685154
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-11-03 11:52:38 +0700

    fix(theme): patch 404 page
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- source/themes/blank/templates/content-none.php
+++ source/themes/blank/templates/content-none.php
@@ -8,41 +8,20 @@
  * @since    0.2.0
  */
 
-?><section <?php post_class(); ?>>
+?><section <?php post_class( 'content no-results' ); ?>>
 
-	<div class="page-content">
+	<div class="entry-content">
 
 		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'blank' ); ?></p>
 
-		<?php
-		get_search_form();
+		<?php get_search_form(); ?>
 
-		the_widget( 'WP_Widget_Recent_Posts' );
-		?>
+	</div> <!-- .entry-content -->
 
-		<div class="widget widget_categories">
-			<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'blank' ); ?></h2>
-			<ul>
-				<?php
-				wp_list_categories( [
-					'orderby'    => 'count',
-					'order'      => 'DESC',
-					'show_count' => 1,
-					'title_li'   => '',
-					'number'     => 10,
-				] );
-				?>
-			</ul>
-		</div> <!-- .widget -->
+	<footer class="entry-footer">
 
-		<?php
-		/* translators: %1$s: smiley */
-		$blank_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'blank' ), convert_smilies( ':)' ) ) . '</p>';
-		the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$blank_archive_content" );
+		<?php blank( 'content' )->footer(); ?>
 
-		the_widget( 'WP_Widget_Tag_Cloud' );
-		?>
-
-	</div> <!-- .page-content -->
+	</footer> <!-- .entry-footer -->
 
 </section> <!-- .no-results -->
