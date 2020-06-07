ab3a42b290bcbcd47b9d4306315c0c8155e9617f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-08 03:09:44 +0700

    fix: fix self-signed ssl issue while development
    See https://stackoverflow.com/a/62249579/881743
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- public/app/mu-plugins/index.php
+++ public/app/mu-plugins/index.php
@@ -1,2 +1,16 @@
 <?php
 // Silence is golden.
+
+/**
+ * Lil hack to get valid self-signed SSL while development.
+ * @see https://stackoverflow.com/a/62249579/881743
+ */
+add_filter( 'http_request_args', function ( $args ) {
+    if ( getenv( 'WP_ENV' ) !== 'development' ) {
+        return $args;
+    }
+
+    $args['sslcertificates'] = ini_get( 'curl.cainfo' ) ?? $args['sslcertificates'];
+
+    return $args;
+}, 0, 1 );
