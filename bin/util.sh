b4d85882c8f3108e74a1ddc056d0ed59632552ad
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-08 05:32:00 +0700

    chore(buildpack): dont use cache_dir just yet
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- bin/util.sh
+++ bin/util.sh
@@ -67,13 +67,10 @@ export_env_dir() {
 
 create_default_env() {
     local build_dir="$1"
-    local cache_dir="$2"
 
-    if [ ! -d "$cache_dir/wp-cli" ]; then
-        mkdir -p "$cache_dir/wp-cli/{bin,cache,packages}"
+    if [ ! -d "$build_dir/wp-cli" ]; then
+        mkdir -p "$build_dir/wp-cli/{bin,cache,packages}"
     fi
-
-    ln -s $cache_dir/wp-cli $build_dir/.heroku/wp-cli
 
     # if the build dir is not "/app", we symlink in the .heroku/wp-cli subdir
     # (and only that, to avoid problems with other buildpacks) so that PHP correctly finds its INI files etc
