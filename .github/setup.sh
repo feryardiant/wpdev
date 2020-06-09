da48aa3923f2520667b22f9607cfa2759030bb86
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-10 06:33:47 +0700

    feat(ci): use wp-cli phar
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- .github/setup.sh
+++ .github/setup.sh
@@ -27,10 +27,14 @@ EOF
 fi
 
 # _inf 'Installing WP-CLI...'
+wpcli_url='https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
+curl_retry --fail --silent --location -o vendor/bin/wp "$wpcli_url" || {
+error <<-EOF
+Failed to download a WP-CLI executable for bootstrapping!
+EOF
+}
 
-# download_wpcli vendor/bin/wp
-
-# chmod +x vendor/bin/wp
+chmod +x vendor/bin/wp
 
 wp() {
     # Here we still using WP-CLI from composer because of some it's commands
@@ -39,19 +43,27 @@ wp() {
     vendor/bin/wp "$@"
 }
 
-# _suc 'WP-CLI installed on vendor/bin/wp'
-
 cp wp-cli.yml wp-cli.local.yml
 
-# sed -i -E "s~url: .*~url: ${WP_HOME}~" wp-cli.local.yml
+cat >> .env <<-EOL
+DB_NAME=wordpress
+DB_USER=root
+DB_PASSWORD=secret
+DB_HOST=$DB_HOST
 
-_inf 'Setting up .env file...'
-cp .env.example .env
+WP_ENV=$WP_ENV
+WP_HOME=$WP_HOME
+WP_DEFAULT_THEME=blank-child
 
-wp dotenv salts generate --color
-wp dotenv set WP_ENV $WP_ENV --color
-wp dotenv set WP_HOME $WP_HOME --color
-wp dotenv set DB_HOST $DB_HOST --color
+AUTH_KEY='ffNM?:/+}9/}H*L/!#k*<sh{tzCz>?c*BZ;xx_/Qm/9GafYoOY:j{K/C{{Zf59o#'
+SECURE_AUTH_KEY='TxZ4r1^NNeYsPX5?mcM.MB_:XH57zX^5<u)uX.Mi[iRN59qm-:.^d#6wINr[za[N'
+LOGGED_IN_KEY='/i:|;37RVJ/ffeT6Q?Uc]!@WK%PcEvuN>wofK3N]k_T58/AOiUp4yU7-T5+<F/iY'
+NONCE_KEY='AXBYCMr/UK^N*f)=4sn=aPu4pkvv,/nfb%,^b6-4Srw#:c^D)/JzI;A/XS,r.dUl'
+AUTH_SALT='Dyh:{PXYIgnqZ/[C}vhuV14bDnZC8G_C?XEmg.Nm4*>*2!BcGT0:}rMbH1:md&V6'
+SECURE_AUTH_SALT='p3eW{YPpM[9)0T&VjIntxTimSypI:jZ1pjZ-aciCSz0U3cN9EGbVk0Wp]JSv0Rb:'
+LOGGED_IN_SALT=',n9-,v,B&siw22:v<T8mxj#%jPQr;S%M:F.#w{eDHCA[?@Qel<_vMF[:zzjr9W-:'
+NONCE_SALT='W3HZnqL9Ar<,PZZ8JUrf2WkO*r5DW!A|X[6Dx:tj4w[C6*;oGk*.K3/I45Upe64!'
+EOL
 
 _inf 'Installling WordPress...'
 wp core install --skip-email --url="${WP_HOME}" --title="WordPress Local" \
@@ -59,10 +71,10 @@ wp core install --skip-email --url="${WP_HOME}" --title="WordPress Local" \
 
 # Feel free to setup your own wp_options
 wp option update permalink_structure '/%postname%/'
-# wp option update link_manager_enabled '1'
+wp option update link_manager_enabled '1'
 
 # _inf 'Import dummy content'
-wp import tests/dummy-content.xml --authors=tests/dummy-content.mapping.csv
+wp import tests/dummy-content.xml --authors=tests/dummy-content.mapping.csv --skip=attachment
 
-# wp menu location assign primary primary
-# wp menu location assign footer footer
+wp menu location assign primary primary
+wp menu location assign footer footer
